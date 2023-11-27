f862aa1f9d0d3b1db32bcf0333f8b76d777a695d
Ty <justfacey@gmail.com>
2023-11-26 20:09:36 -0500

    Implemented scan() along with a it's helper functions.

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,5 +1,19 @@
 package cmd
 
+import (
+	"fmt"
+	. "loco-moco/internals"
+)
+
+func recursiveScanFolder(folder string) []string {
+	return ScanGitFolders(make([]string, 0), folder)
+}
+
 func scan(path string) {
-	print("scan")
+	fmt.Printf("Found folders: \n\n")
+	repos := recursiveScanFolder(path)
+	filePath := GetDotFilePath()
+	AddNewSliceElementsToFile(filePath, repos)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+
 }
